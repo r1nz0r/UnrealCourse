57ee5ac06b7e30a720eb0cbd40246f00b7eaeea3
r1nz0r <nicecontra1@gmail.com>
2025-01-28 16:06:35 +0300

    Removed default comments from PlatformInvocator.h

--- XYZ_Course/Source/XYZ_Course/Actors/PlatformInvocator.h
+++ XYZ_Course/Source/XYZ_Course/Actors/PlatformInvocator.h
@@ -14,16 +14,14 @@ class XYZ_COURSE_API APlatformInvocator : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	APlatformInvocator();
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
+	//Needs to be called from blueprint child class
 	UFUNCTION(BlueprintCallable)	
 	void Invoke();
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	
 	FOnPlatformInvocatorActivated OnPlatformInvocatorActivated;
